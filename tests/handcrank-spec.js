cffb03330bfaa9b01411c16745e1195259ef5db2
Marcos Caceres <marcos@marcosc.com>
2016-07-10 14:28:41 +1000

    feat(handcrank-spec): add Handcrank tests

---
+++ tests/handcrank-spec.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+
+const Handcrank =
+  require("../lib/handcrank");
+
+describe("handcrank", () => {
+
+  it("is contructable", () => {
+    const hk = new Handcrank();
+    expect(hk).to.be.instanceof(Handcrank);
+  });
+
+  describe("data method", () => {
+
+  });
+
+  describe("add method", () => {
+
+  });
+})
