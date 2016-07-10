915a8629071dec5be42a39e733dff73fc22229ee
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-09 23:40:44 -0700

    chore(all): changed quotes to make eslint happy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+'use strict';
 const SerialPort = require('serialport');
-const ET312 = require("./lib/ET312");
+const ET312 = require('./lib/ET312');
 const opts = {
   baudrate: 19200,
   // If only we had a viable parser to use
@@ -11,17 +12,17 @@ const port = new SerialPort('/dev/ttyS0', opts);
 port.on('open', function() {
   var e = new ET312(port);
   e.handshake().then(function() {
-    console.log("handshake succeeded!");
+    console.log('handshake succeeded!');
     e.key_exchange().then(function() {
-      console.log("Box key: " + e._key);
+      console.log('Box key: ' + e._key);
       port.close();
     }, function(err) {
-      console.log("key exchange failed!");
+      console.log('key exchange failed!');
       console.log(err);
       port.close();
     });
   }, function(err) {
-    console.log("handshake failed!");
+    console.log('handshake failed!');
     console.log(err);
     port.close();
   });
